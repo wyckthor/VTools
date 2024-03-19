611a189204896e93374fc783bac9a0f1e7c9a1be
Victor Marques Honório <victor.honorio@gmail.com>
2024-03-19 13:10:16 -0300

    Update Singleton.cs

--- Scripts/Singleton.cs
+++ Scripts/Singleton.cs
@@ -6,21 +6,36 @@ namespace VHon
     {
         //===============================================================================================================================
         // internal static Singleton Instance;
-        // void Awake() => Instance = this.TrySingleton();
+        // void Awake() => Instance = this.TrySingleton(Instance);
         //===============================================================================================================================
-        public static T TrySingleton<T>(this T instance) where T : MonoBehaviour
-        {
-            T newInstance = null;
+        // public static T TrySingleton<T>(this T instance) where T : MonoBehaviour
+        // {
+        //     T newInstance = null;
 
-            if (instance != null)
+        //     if (instance != null)
+        //     {
+        //         if (Object.FindObjectsOfType<T>().Length == 1)
+        //         {
+        //             Object.DontDestroyOnLoad(instance.gameObject);
+        //             newInstance = instance;
+        //         }
+        //         else Object.Destroy(instance.gameObject);
+        //     }
+        //     return newInstance;
+        // }
+
+
+        public static T TrySingleton<T>(this T obj, T Instance) where T : MonoBehaviour
+        {
+            T newInstance = Instance;
+
+            if (Instance == null)
             {
-                if (Object.FindObjectsOfType<T>().Length == 1)
-                {
-                    Object.DontDestroyOnLoad(instance.gameObject);
-                    newInstance = instance;
-                }
-                else Object.Destroy(instance.gameObject);
+                newInstance = obj;
+                Object.DontDestroyOnLoad(obj.gameObject);
             }
+            else if (Instance != obj) Object.Destroy(obj.gameObject);
+
             return newInstance;
         }
         // ------------------------------------------------------------------------------------------------------------------------------        
